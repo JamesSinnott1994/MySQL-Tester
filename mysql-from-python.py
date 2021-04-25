88b217bb573472b293b8c0b5708f528332122258
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-04-25 11:25:33 +0000

    'cursor.executemany' on list of tuples

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -2,21 +2,22 @@ import os
 import datetime
 import pymysql
 
-# Get username from Cloud9 workspace
+# Get the username from the Cloud9 workspace
 # (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
 # Connect to the database
-connection = pymysql.connect(host='localhost', user=username, password='', db='Chinook')
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
 
 try:
-    # Run with a query
     with connection.cursor() as cursor:
-        row = ("Bob", 21, "1990-02-06 23:04:56")
-        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        rows = [("bob", 21, "1990-02-06 23:04:56"),
+                ("jim", 56, "1955-05-09 13:12:45"),
+                ("fred", 100, "1911-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s,%s,%s);", rows)
         connection.commit()
-        # Note that the above will still display a warning (not error) if the
-        # table already exists
 finally:
-    # Close the connection, regardless of whether the above was successful
     connection.close()
